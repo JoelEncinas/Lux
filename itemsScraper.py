d4c61f54c0937db4c2a9f7ab5408f47134fb23a6
JoelEncinas <joelencinasmartin@gmail.com>
2023-02-09 18:27:12 +0100

    added web scrapper

---
+++ itemsScraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/assets/items/icons2d/"
+
+html_content = requests.get(url).text
+soup = BeautifulSoup(html_content, "html.parser")
+
+items = []
+for link in soup.find_all("a"):
+    item = link.get("href")
+    if item and item.endswith(".png"):
+        items.append(item[:-4])
+
+print(items)
